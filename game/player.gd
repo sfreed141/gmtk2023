fd895cbaf146a25dfd17f942e408ba1b11f04a50
Sam Freed <sfreed141@gmail.com>
2023-07-07 13:12:18 -0700

    top down 2d foundation

---
+++ game/player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+class_name Player
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction.normalized() * SPEED
+	move_and_slide()
