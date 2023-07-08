8ec23aa2d6569aff6bfe1c673064c79b745e4a88
Sam Freed <sfreed141@gmail.com>
2023-07-07 22:14:09 -0700

    add 128x128 sprite (#3)

--- game/unit_bar.gd
+++ game/unit_bar.gd
@@ -1,6 +1,6 @@
 extends Control
 
-signal place_unit(unit_data: UnitData, unit_position: Vector2)
+signal place_unit(unit_data: UnitData, unit_local_position: Vector2)
 
 @onready var _grid_container: GridContainer = $MarginContainer/GridContainer
 @onready var _selected_unit: PanelContainer = $SelectedUnit
@@ -23,23 +23,21 @@ func _on_slot_clicked(index: int, button: int):
 	_selected_unit.unit_data = unit_data
 	_selected_unit.visible = true
 
-func _get_snapped_world_mouse_position():
-	var viewport_position = get_viewport().get_mouse_position()
-	var world_position = _world.get_canvas_transform().inverse() * viewport_position
+func _get_snapped_world_local_mouse_position():
+	var world_position = _world.get_local_mouse_position()
 	return world_position.snapped(TILE_DIMENSION)
 
-func _get_snapped_global_mouse_position():
-	# viewport_position (from get_viewport().get_mouse_position()) is relative to the top-left corner of the window,
-	# but we want to snap to the world/level grid.
-	# So we transform it relative to the world's canvas, apply the snapping, and then transform back
-	var world_position = _get_snapped_world_mouse_position()
-	return _world.get_canvas_transform() * world_position
+func get_snapped_viewport_mouse_position():
+	var world_position = _get_snapped_world_local_mouse_position()
+	return _world.get_global_transform_with_canvas() * world_position
+
 
 func _physics_process(delta: float) -> void:
 	if _selected_unit.visible:
-		_selected_unit.global_position = _get_snapped_global_mouse_position()
+		var viewport_position = get_snapped_viewport_mouse_position()
+		_selected_unit.global_position = get_canvas_transform().inverse() * viewport_position
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		var world_position = _get_snapped_world_mouse_position()
+		var world_position = _get_snapped_world_local_mouse_position()
 		place_unit.emit(_selected_unit.unit_data, world_position)
