862a82079fb60ef1b2b453afc44c45f8b968e86b
Sam Freed <sfreed141@gmail.com>
2023-07-07 22:26:37 -0700

    hero sprite sheets (animated)

--- game/hero.gd
+++ game/hero.gd
@@ -8,6 +8,11 @@ const SPEED = 100.0
 @export var path: Path2D
 var _path_index := 0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	animated_sprite_2d.speed_scale = 2
+
 func _physics_process(delta: float) -> void:
 	var direction = path.curve.get_point_position(_path_index) - global_position
 	if direction.length() < 10:
@@ -16,6 +21,11 @@ func _physics_process(delta: float) -> void:
 			path_finished.emit()
 			set_physics_process(false)
 	direction = direction.normalized()
+	if direction.y > 0:
+		animated_sprite_2d.play("fwd")
+	else:
+		animated_sprite_2d.play("bkd")
+	
 	velocity = direction * SPEED
 	move_and_slide()
 	
