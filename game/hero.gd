5fd8d8600b563dcbfdbcdd56ffbc7097a79fb8c9
Sam Freed <sfreed141@gmail.com>
2023-07-08 14:22:02 -0700

    hero stops and attacks spinny axe unit

--- game/hero.gd
+++ game/hero.gd
@@ -7,12 +7,18 @@ signal health_changed(hp)
 var health = 100
 
 const SPEED = 100.0
+const ATTACK_RANGE = 10
+
+var xp = 0
+var attack_damage = 20	# TODO increase on level up
 
 @export var path: Path2D
 var _path_index := 0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var navigation_agent_2d = $NavigationAgent2D
+
+var target_unit: Node2D
 
 func _ready() -> void:
 	animated_sprite_2d.speed_scale = 2
@@ -27,8 +33,25 @@ func nav_setup():
 	navigation_agent_2d.target_position = target_position
 	set_physics_process(true)
 
+func do_attack():
+	print("WACK")
+	target_unit.hit(attack_damage)
+	if target_unit.health <= 0:
+		xp += target_unit.xp_granted
+		target_unit.queue_free()
+		target_unit = null
+
 func _physics_process(delta: float) -> void:
-	if navigation_agent_2d.target_position.distance_to(global_position) < 50:
+	if $AnimationPlayer.is_playing():
+		return
+	
+	if target_unit:
+		if target_unit.global_position.distance_to(global_position) < ATTACK_RANGE:
+			# todo attack. on defeat, gain xp and clear target_unit
+			$AnimationPlayer.play("attack")
+		else:
+			move_towards(target_unit.global_position)
+	elif navigation_agent_2d.target_position.distance_to(global_position) < 50:
 		path_finished.emit()
 		set_physics_process(false)
 	else:
@@ -36,24 +59,29 @@ func _physics_process(delta: float) -> void:
 		$Line2D.add_point(next_position)
 		
 		if navigation_agent_2d.is_target_reachable():
-			var direction = next_position - global_position
-			
-			direction = direction.normalized()
-			if direction.y > 0:
-				animated_sprite_2d.play("fwd")
-			else:
-				animated_sprite_2d.play("bkd")
-			
-			velocity = direction * SPEED
-			move_and_slide()
+			move_towards(next_position)
 		else:
 			# if path is blocked (units in the way) and we're at the final position,
 			# spin attack until the way is clear
 			var final_position = navigation_agent_2d.get_final_position()
 			if final_position.distance_to(next_position) < 10:
 				print("blocked! spin attack")
-		
+
+func move_towards(next_position):
+	var direction = next_position - global_position
+			
+	direction = direction.normalized()
+	if direction.y > 0:
+		animated_sprite_2d.play("fwd")
+	else:
+		animated_sprite_2d.play("bkd")
+	
+	velocity = direction * SPEED
+	move_and_slide()
 	
 func hit(amount):
 	health -= amount
 	health_changed.emit(health)
+
+func _on_aggro_range_area_entered(area):
+	target_unit = area.owner
