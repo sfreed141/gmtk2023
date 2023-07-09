28511aa8bca0965a73193ebf54e30ec1d21c8600
Gwillis442 <122920316+Gwillis442@users.noreply.github.com>
2023-07-08 19:33:31 -0700

    Added 3 new levels (#15)
    * Added new tile maps
    
    * Added level 1 tile map
    
    * added level 2 and fixed level 1
    
    * Update level_2.tscn
    
    * updated level 2 and made level 3
    
    * Update level_2.tscn
    
    * added pathing to level 2 and 3
    
    * updated levels
    
    ---------
    
    Co-authored-by: Sam Freed <sfreed141@gmail.com>

--- game/units/spinny_axe_unit.gd
+++ game/units/spinny_axe_unit.gd
@@ -2,6 +2,7 @@ extends Unit
 
 
 const ROTATION_SPEED_SCALE = 3
+const DAMAGE = 20
 
 @onready var pivot: Node2D = $Pivot
 
@@ -15,4 +16,7 @@ func _process(delta: float) -> void:
 
 func _on_hurt_box_body_entered(body: Node2D) -> void:
 	if body.has_method("hit"):
-		body.hit(damage)
+		body.hit(DAMAGE)
+
+func hit(amount):
+	health -= amount
